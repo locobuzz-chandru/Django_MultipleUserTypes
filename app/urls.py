c4bc8e0d3c0f4fe12dc0bfed59d913f4bf68dc83
unknown <chandru151998@gmail.com>
2023-06-13 19:56:40 +0530

    [Chandru]Adde.custome function for adding permissions

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,8 @@
-from django.urls import path, include
+from django.urls import path
 from app import views
-from app.permissions import create_teacher_group
 
 urlpatterns = [
     path('', views.root),
     path('teacher', views.add_teacher),
     path('student', views.add_student),
-    path('perm', create_teacher_group)
 ]
